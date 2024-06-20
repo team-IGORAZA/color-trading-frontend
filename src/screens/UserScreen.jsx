526116339bd0c66415fd781f95b2dac0a48a899f
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-06-20 22:57:55 +0530

    feat(user screen)

--- src/screens/UserScreen.jsx
+++ src/screens/UserScreen.jsx
@@ -1,26 +1,39 @@
 
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faNoteSticky, faRightFromBracket, faShield, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faWallet } from '@fortawesome/free-solid-svg-icons/faWallet'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import style from '../styles/userScreen.module.css'
 
 function UserScreen() {
     return (
-        <div>
-            <img src="" alt="" />
-            <p>Anirudh MK</p>
-            <p>anirudhmk123@gmail.com</p>
-            <div>
-                <FontAwesomeIcon icon={faUser} />
-                <p>Edit Profile</p>
+        <div className={style.container}>
+            <div className={style.topContainer}>
+                <img className={style.profilePic} src="" alt="" />
+                <p>Anirudh MK</p>
+                <p>anirudhmk123@gmail.com</p>
             </div>
-            <div>
-                <FontAwesomeIcon icon={faWallet} />
-                <p>Wallet</p>
+            <div className={style.bottomContainer}>
+                <div className={style.button}>
+                    <FontAwesomeIcon icon={faUser} />
+                    <p>Edit Profile</p>
+                </div>
+                <div className={style.button}>
+                    <FontAwesomeIcon icon={faWallet} />
+                    <p>Wallet</p>
+                </div>
+                <div className={style.button}>
+                    <FontAwesomeIcon icon={faNoteSticky} />
+                    <p>Terms and Conditions</p>
+                </div>
+                <div className={style.button}>
+                    <FontAwesomeIcon icon={faShield} />
+                    <p>Privacy and Security</p>
+                </div>
             </div>
-            <div>
-                <FontAwesomeIcon icon={faUser} />
-                <p>Edit Profile</p>
+            <div className={style.logout}>
+                <FontAwesomeIcon icon={faRightFromBracket} />
+                <p>Logout</p>
             </div>
         </div>
     )
