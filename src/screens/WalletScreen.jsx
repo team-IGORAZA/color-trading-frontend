15d77b3b4ec0e207df59030c4c58036880789498
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-06-21 11:05:49 +0530

    feat(wallet screen)

--- src/screens/WalletScreen.jsx
+++ src/screens/WalletScreen.jsx
@@ -3,23 +3,55 @@ import styles from '../styles/walletScreen.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function WalletScreen() {
     return (
-        <div className={styles.container}>
-            <h1 className={styles.headding}>Earnings</h1>
-            <div className={styles.balance}>
-                <h2>My Balance</h2>
-                <br />
-                <h2>Rs 10000000</h2>
-            </div>
-            <div className={styles.buttonContainer}>
-                <div className={styles.withdrawButton}>
-                    <p>Withdraw</p>
+        <>
+            <div className={styles.topContainer}>
+                <h2>Earnings</h2>
+                <div className={styles.balance}>
+                    <p>Balance</p>
+                    <p>200000 Rs</p>
                 </div>
-                <div className={styles.depositButton}>
-                    <p>Deposit</p>
+                <div className={styles.buttonContainer}>
+                    <div className={styles.withdrawButton}><p>Withdraw</p></div>
+                    <div className={styles.depositButton}><p>Deposit</p></div>
+                </div>
+                <h2>Transactions</h2>
+            </div>
+            <div className={styles.buttomContainer}>
+                <div className={styles.detailsContainer}>
+                    <div className={styles.headdingContainer}>
+                        <p>Withdraw</p>
+                        <p>Success</p>
+                    </div>
+                    <div className={styles.subHeaddingContainer}>
+                        <p>200</p>
+                        <p>20-10-2024</p>
+                    </div>
+                    <p></p>
+                </div>
+                <div className={styles.detailsContainer}>
+                    <div className={styles.headdingContainer}>
+                        <p>Withdraw</p>
+                        <p>Success</p>
+                    </div>
+                    <div className={styles.subHeaddingContainer}>
+                        <p>200</p>
+                        <p>20-10-2024</p>
+                    </div>
+                    <p></p>
+                </div>
+                <div className={styles.detailsContainer}>
+                    <div className={styles.headdingContainer}>
+                        <p>Withdraw</p>
+                        <p>Success</p>
+                    </div>
+                    <div className={styles.subHeaddingContainer}>
+                        <p>200</p>
+                        <p>20-10-2024</p>
+                    </div>
+                    <p></p>
                 </div>
             </div>
-            <h2 className={styles.subHeadding}>Transactions</h2>
-        </div>
+        </>
     )
 }
 
