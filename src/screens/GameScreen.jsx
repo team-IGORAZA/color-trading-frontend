cb04720f032ba33a912d1054dc352b0b97f0baaa
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-06-21 11:32:36 +0530

    feat(gameScreen map function added)

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import style from '../styles/gameScreen.module.css'
 import GameCard from '../widgets/GameCard'
 
+import supportFile from '../utils/supportFiles'
 function GameScreen() {
     return (
         <div className={style.container}>
-            <GameCard />
+            {supportFile.map(
+                (item, index) => <GameCard
+                    key={index}
+                    img={item.img}
+                    name={item.name}
+                    price={item.price}
+                    noOfParticipents={item.no_of_participents}
+                    timeLeft={item.time_left}
+                />)}
         </div>
     )
 }
