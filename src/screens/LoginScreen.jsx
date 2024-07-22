ce4e290c4acf66c2b6d28388baffda8fa0570cf5
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-07-22 11:20:34 +0530

    feat(loginScreen)

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react'
 import style from '../styles/loginScreen.module.css'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 function LoginScreen() {
     const navigate = useNavigate()
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-
+    const [error, setError] = useState('')
     const handleLogin = (e) => {
         e.preventDefault();
-        navigate('dashboard/game')
+
+        axios.post('http://127.0.0.1:8000/api/v1/user/login/', {
+            email: email,
+            password: password
+        })
+            .then(function (response) {
+                let accessToken = response.data.response.accessToken
+                if (accessToken) {
+                    navigate('dashboard/game')
+                    localStorage.setItem('assessToken', response.data.response.accessToken);
+                }
+                setError(error.response.data.message.general[0])
+
+            })
+            .catch(function (error) {
+                setError(error.response.data.message.general[0])
+            });
+
+
     }
 
     return (
@@ -23,6 +42,7 @@ function LoginScreen() {
                         <p>Password</p>
                         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
+                    <p className={style.errorText}>{error}</p>
                     <br />
                     <button type='submit' className={style.signInButton}><p>Sign In</p></button>
                 </form>
