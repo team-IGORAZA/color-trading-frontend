6f786e63be7aad8f4a793d1c9c34a9103d19826f
Adarsh <69946307+adarshswaminath@users.noreply.github.com>
2024-07-31 16:54:06 +0530

    add: color selection functionality added need to add the api implementation

--- src/Pages/WinGO1Min/ColorMap.tsx
+++ src/Pages/WinGO1Min/ColorMap.tsx
@@ -1,15 +1,46 @@
-import { useState } from "react"
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { BACKEND_URL } from "../../constants";
 
-export default function ColorMap() {
+
+interface ColorMapProps {
+    setUserSelectedColorID: React.Dispatch<React.SetStateAction<string>>
+}
+
+const ColorMap: React.FC<ColorMapProps> = ({ setUserSelectedColorID }) => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    const getColors = async () => {
+        try {
+            const req = await axios.get(`${BACKEND_URL}/api/v1/color`, {
+                headers: { Authorization: `Bearer ${accessToken}` }
+            });
+            console.log(req.data.response);
+        } catch (error) {
+            console.error("Error fetching colors:", error);
+        }
+    }
+
+
+    useEffect(() => {
+        getColors()
+    }, [])
     const [selectedButton, setSelectedButton] = useState(0)
     return (
         <section className="mt-6 mb-6 flex  w-full max-w-4xl gap-4">
-            <button onClick={() => setSelectedButton(0)}
+            <button onClick={() => {
+                setUserSelectedColorID("")
+                setSelectedButton(0)
+            }}
                 className={`text-nowrap py-4 text-white font-bold p-2 w-full rounded-bl-3xl border ${selectedButton === 0 ? "bg-gradient" : "bg-red-500"}`}>
                 {selectedButton === 0 ? "Selected Red" : "Choose Your Color Red"}
             </button>
             <button
-                onClick={() => setSelectedButton(1)}
+                onClick={() => {
+                    setUserSelectedColorID("")
+                    setSelectedButton(1)
+                }}
                 className={`text-nowrap py-4 p-2 w-full text-white font-bold rounded-br-3xl border ${selectedButton === 1 ? "bg-gradient" : "bg-green-500"}`}>
                 {selectedButton === 1 ? "Selected Green" : "Choose Your Color Green"}
             </button>
@@ -17,3 +48,4 @@ export default function ColorMap() {
         </section>
     )
 }
+export default ColorMap
--- src/Pages/WinGO1Min/WinGO1Min.tsx
+++ src/Pages/WinGO1Min/WinGO1Min.tsx
@@ -1,18 +1,14 @@
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import TimeBanner from "./TimeBanner";
 import ColorMap from "./ColorMap";
+import { useState } from "react";
 
 
-const Bonus = () => (
-    <div
-        style={{ background: "linear-gradient(90deg, #FDF541 0%, #FB9B2A 100%)" }}
-        className="mt-6 mb-5 w-full p-2 max-w-6xl rounded-xl py-2 text-center">
-        <span className="capitalize text-center text-black font-medium">10 % Bonus for the first recharge</span>
-    </div>
-)
 
 
 export default function WinGO1Min() {
+    const [userSlectedColorId,setUserSelectedColorID] = useState('')
+    console.log(userSlectedColorId)
     return (
         <main className="p-2">
             <header className="flex items-center space-x-4">
@@ -24,10 +20,19 @@ export default function WinGO1Min() {
             <section className="flex flex-col justify-center items-center p-3">
                 <Bonus />
                 <TimeBanner />
-                <ColorMap/>
+                <ColorMap setUserSelectedColorID={setUserSelectedColorID}/>
                 {/* ? participate button */}
                 <button className="btn mt-6 w-full max-w-4xl btn-bg text-black shadow-xl border-none">Participate</button>
             </section>
         </main>
     )
 }
+
+
+const Bonus = () => (
+    <div
+        style={{ background: "linear-gradient(90deg, #FDF541 0%, #FB9B2A 100%)" }}
+        className="mt-6 mb-5 w-full p-2 max-w-6xl rounded-xl py-2 text-center">
+        <span className="capitalize text-center text-black font-medium">10 % Bonus for the first recharge</span>
+    </div>
+)
